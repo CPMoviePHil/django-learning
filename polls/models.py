e62e1766bb98cd65566f6ceb0f077e9573e2d805
黃智輝 <cpmoviephil@gmail.com>
2020-05-20 17:49:56 +0800

    在models定義好兩個table的內容

--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    pass
+    question_text = models.CharField(max_length=200)
+    publication_date = models.DateTimeField()
 
 
 class Choice(models.Model):
-    pass
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
